09af17d548fc9df4b8e2a33e046d22225e1191b2
Mhd Nur Ifaturachman <ifuku.007@gmail.com>
2026-05-23 11:26:12 +0700

    debug vercel shop render

---
+++ src/app/test-shop/page.tsx
@@ -0,0 +1,7 @@
+export default function TestShopPage() {
+  return (
+    <div style={{ padding: 40, color: "white", background: "#020617", minHeight: "100vh" }}>
+      <h1>Test Shop Render Berhasil</h1>
+    </div>
+  );
+}
